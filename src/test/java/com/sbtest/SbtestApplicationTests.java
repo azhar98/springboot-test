75146b3d464c776d49ea10dce52b6be04e157763
azhar98 <azharuddin.mondal98@gmail.com>
2025-11-29 08:24:36 +0530

    initilied commit

---
+++ src/test/java/com/sbtest/SbtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
