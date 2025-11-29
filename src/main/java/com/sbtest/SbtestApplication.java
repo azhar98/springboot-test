462c6774af0160cbcdacfd1ae8c4942d568b2c12
azhar98 <azharuddin.mondal98@gmail.com>
2025-11-29 09:24:54 +0530

    updated main file

--- src/main/java/com/sbtest/SbtestApplication.java
+++ src/main/java/com/sbtest/SbtestApplication.java
@@ -8,7 +8,7 @@ public class SbtestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbtestApplication.class, args);
-		System.out.println("hello world");
+		System.out.println("hello worldg");
 	}
 
 }
